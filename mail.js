23a966ea4cd2e076a8af45ab12a16bce20151d6a
oussama <maoussaberhayla@gmail.com>
2021-04-11 13:38:01 +0000

    ouss

---
+++ mail.js
@@ -0,0 +1,7 @@
+
+function msgshow(){
+    var y=document.getElementById('list-msg');
+    y.innerHTML+='<li> <img src="img/1.png" alt="">  <div class="text"><div class="text-top"> <p>oussama berhayala</p><span>2 houres</span></div> <div class="text-bottom"> <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, magnam! </p></div></div></li>';
+   
+}
+
